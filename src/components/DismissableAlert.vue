9390bd71b3d4454813121533a498f0dc6d077021
Andrea Brioschi <andreabrioschi@MacBook-Air-di-Andrea.local>
2021-06-20 17:46:12 +0200

    Response alert

---
+++ src/components/DismissableAlert.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    :class="[
+      'alert alert-dismissible fade show',
+      status,
+      success ? 'alert-success' : 'alert-danger',
+    ]"
+    role="alert"
+  >
+    {{ message }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "DismissableAlert",
+  props: ["message", "status", "success"],
+};
+</script>
--- src/components/GroupNotificationForm.vue
+++ src/components/GroupNotificationForm.vue
@@ -1,56 +1,73 @@
 <template>
-  <form id="GroupNotificationForm">
-    <div class="form-group">
-      <label for="message-title">Titolo</label>
-      <input
-        type="text"
-        class="form-control"
-        id="message-title"
-        aria-describedby="title-hint"
-        placeholder="Titolo della notifica"
-        v-model="title"
-      />
-      <small id="title-hint" class="form-text text-muted"
-        >E' possibile utilizzare le keywords sia nel titolo che nel corpo della
-        notifica.</small
+  <div>
+    <DismissableAlert
+      :message="alert.message"
+      :status="alert.status"
+      :success="alert.success"
+    ></DismissableAlert>
+    <form id="GroupNotificationForm">
+      <div class="form-group">
+        <label for="message-title">Titolo</label>
+        <input
+          type="text"
+          class="form-control"
+          id="message-title"
+          aria-describedby="title-hint"
+          placeholder="Titolo della notifica"
+          v-model="title"
+        />
+        <small id="title-hint" class="form-text text-muted"
+          >E' possibile utilizzare le keywords sia nel titolo che nel corpo
+          della notifica.</small
+        >
+      </div>
+      <div class="form-group">
+        <label for="message-body">Corpo</label>
+        <textarea
+          type="text"
+          class="form-control"
+          id="message-body"
+          placeholder="Corpo della notifica"
+          v-model="body"
+        >
+        </textarea>
+      </div>
+      <button
+        type="button"
+        @click="sendNotification"
+        class="btn btn-primary rounded"
       >
-    </div>
-    <div class="form-group">
-      <label for="message-body">Corpo</label>
-      <textarea
-        type="text"
-        class="form-control"
-        id="message-body"
-        placeholder="Corpo della notifica"
-        v-model="body"
-      >
-      </textarea>
-    </div>
-    <button
-      type="button"
-      @click="sendNotification"
-      class="btn btn-primary rounded"
-    >
-      Invia Notifica
-    </button>
-  </form>
+        Invia Notifica
+      </button>
+    </form>
+  </div>
 </template>
 <script>
+import DismissableAlert from "../components/DismissableAlert";
+
 export default {
   name: "GrousNotificationFrom",
+  components: {
+    DismissableAlert,
+  },
   props: ["groupId"],
   data: function () {
     return {
       id: this.groupId,
       title: "",
       body: "",
+      alert: {
+        message: "",
+        status: "d-none",
+        success: true,
+      },
     };
   },
   methods: {
     async sendNotification() {
       const accessToken = await this.$auth.getTokenSilently();
       try {
-        await this.$http.post(
+        let response = await this.$http.post(
           "https://push-api.herokuapp.com/messages/group",
           {
             group: this.id,
@@ -63,10 +80,23 @@ export default {
             },
           }
         );
+
+        this.showResponseAlert(
+          `Operazione completata, ${
+            Object.keys(response.data.response).length
+          } notifiche inviate con successo.`
+        );
       } catch (e) {
-        console.error(e);
-        //   this.apiMessage = `Error: the server responded with '${e.response.status}: ${e.response.statusText}'`;
+        this.showResponseAlert(
+          `Errore: il server ha risposto con '${e.response.status}: ${e.response.statusText}'`,
+          false
+        );
       }
+    },
+    showResponseAlert(message, success = true) {
+      this.alert.message = message;
+      this.alert.status = "d-block";
+      this.alert.success = success;
     },
   },
 };
